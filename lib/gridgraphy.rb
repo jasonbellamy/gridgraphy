a4075af0a50bcef4c997bdb978abf9fe5655bef2
Jason Bellamy <j@sonbellamy.com>
2015-06-03 11:13:16 -0400

    removed ruby/compass specific files

--- lib/gridgraphy.rb
+++
@@ -1,9 +0,0 @@
-require "gridgraphy/version"
-require 'compass'
-
-Compass::Frameworks.register('gridgraphy',
-                             :stylesheets_directory => File.join(File.dirname(__FILE__), '..', 'stylesheets'),
-                             :templates_directory => File.join(File.dirname(__FILE__), '..', 'templates'))
-
-
-Sass::Script::Number.precision = 7
--- lib/gridgraphy/version.rb
+++
@@ -1,3 +0,0 @@
-module Gridgraphy
-  VERSION = "0.2.0"
-end
